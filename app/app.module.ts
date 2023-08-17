b312222a0911671f66b7dd5d0d67d0073bbcd51b
Rodrigo Solis <solisrodrigo619@gmail.com>
2023-08-17 13:46:16 -0300

    AgregarTarjeta

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreditCardComponent } from './components/credit-card/credit-card.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CreditCardComponent } from './components/credit-card/credit-card.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app/components/credit-card/credit-card.component.ts
+++ app/components/credit-card/credit-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-credit-card',
@@ -6,19 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./credit-card.component.css']
 })
 export class CreditCardComponent {
+
+  agregarTarjeta(){
+    console.log(this.form)
+
+    const tarjeta: any ={
+      titular: this.form.get('titular')?.value,
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+      fechaExpiracion: this.form.get('fechaExpiracion')?.value,
+      cvv: this.form.get('cvv')?.value
+    
+    }
+
+    this.listTarjetas.push(tarjeta)
+    this.form.reset();
+    }
+
+
   listTarjetas: any[] = [
     {
-      titulo: "Juan Perez",
+      titular: "Juan Perez",
       numeroTarjeta: '23232323',
       fechaExpiracion: '11/25',
       cvv: '123'
     },
     {
-      titulo: "Ramiro Perez",
+      titular: "Ramiro Perez",
       numeroTarjeta: '25252525',
       fechaExpiracion: '12/25',
       cvv: '321'
     }
   ];
 
+ form: FormGroup;
+
+ constructor(private fb: FormBuilder){
+  this.form = this.fb.group({
+    titular:[''],
+    numeroTarjeta:[''],
+    fechaExpiracion:[''],
+    cvv:['']
+  })
+ }
+
+
+
+
 }
+
